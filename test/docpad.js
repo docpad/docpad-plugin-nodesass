2059d25f20542e8994c8d9bda14780f845013bef
Michael Barlock <mbarlock@us.ibm.com>
2015-05-05 16:28:34 -0400

    Update node-sass and allow all node-sass options

--- test/docpad.js
+++ test/docpad.js
@@ -1,11 +1,13 @@
 module.exports = {
   plugins: {
     nodesass: {
-      imagePath: '/path/to/images',
       neat: true,
-      outputStyle: 'compressed',
-      precision: 3,
-      includePaths: [__dirname + "/src/documents/includePath"]
+      options: {
+        imagePath: '/path/to/images',
+        outputStyle: 'compressed',
+        precision: 3,
+        includePaths: [__dirname + "/src/documents/includePath"]
+      }
     }
   }
 };
